e438570277e4fe3e1fac9d0df40c3c589896fd4f
Siroshun09 <siro.shun8@gmail.com>
2019-11-08 16:53:30 +0900

    [Add] Bungee におけるメッセージ関連のメソッド

---
+++ src/main/java/com/github/siroshun09/sirolibrary/message/BungeeMessage.java
@@ -0,0 +1,30 @@
+package com.github.siroshun09.sirolibrary.message;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.jetbrains.annotations.NotNull;
+
+public class BungeeMessage {
+
+    /**
+     * {@code str} の & から始まるカラーコードを変換して返す。
+     *
+     * @param str 変換する文字列
+     * @return 変換後の文字列
+     */
+    @NotNull
+    public static String setColor(@NotNull String str) {
+        return ChatColor.translateAlternateColorCodes('&', str);
+    }
+
+    /**
+     * {@link CommandSender} に {@code msg} を送る
+     *
+     * @param sendTo メッセージの受取人
+     * @param msg    送る文字列
+     */
+    public static void sendMessage(@NotNull CommandSender sendTo, @NotNull String msg) {
+        sendTo.sendMessage(TextComponent.fromLegacyText(msg));
+    }
+}
