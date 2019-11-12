8361f8bf76f7c366bb0402665e5a1013fca755e3
Siroshun09 <siro.shun8@gmail.com>
2019-11-12 18:28:11 +0900

    [Add] Javadoc

--- src/main/java/com/github/siroshun09/sirolibrary/message/BungeeMessage.java
+++ src/main/java/com/github/siroshun09/sirolibrary/message/BungeeMessage.java
@@ -42,6 +42,25 @@ public class BungeeMessage {
         sendTo.sendMessage(TextComponent.fromLegacyText(setColor(msg)));
     }
 
+    /**
+     * <br>このメソッドは {@link org.bukkit.util.StringUtil#copyPartialMatches(String, Iterable, Collection)} からのコピーである</br>
+     * <p>
+     * Copies all elements from the iterable collection of originals to the
+     * collection provided.
+     *
+     * @param <T>        the collection of strings
+     * @param token      String to search for
+     * @param originals  An iterable collection of strings to filter.
+     * @param collection The collection to add matches to
+     * @return the collection provided that would have the elements copied
+     * into
+     * @throws UnsupportedOperationException if the collection is immutable
+     *                                       and originals contains a string which starts with the specified
+     *                                       search string.
+     * @throws IllegalArgumentException      if any parameter is is null
+     * @throws IllegalArgumentException      if originals contains a null element.
+     *                                       <b>Note: the collection may be modified before this is thrown</b>
+     */
     @Contract("_, _, _ -> param3")
     @NotNull
     public static <T extends Collection<? super String>> T copyPartialMatches(@NotNull final String token, @NotNull final Iterable<String> originals, @NotNull final T collection) throws UnsupportedOperationException, IllegalArgumentException {
@@ -58,6 +77,20 @@ public class BungeeMessage {
         return collection;
     }
 
+    /**
+     * <br>このメソッドは {@link org.bukkit.util.StringUtil#startsWithIgnoreCase(String, String)} からのコピーである</br>
+     * <p>
+     * This method uses a region to check case-insensitive equality. This
+     * means the internal array does not need to be copied like a
+     * toLowerCase() call would.
+     *
+     * @param string String to check
+     * @param prefix Prefix of string to compare
+     * @return true if provided string starts with, ignoring case, the prefix
+     * provided
+     * @throws NullPointerException     if prefix is null
+     * @throws IllegalArgumentException if string is null
+     */
     private static boolean startsWithIgnoreCase(@NotNull final String string, @NotNull final String prefix) throws IllegalArgumentException, NullPointerException {
         Validate.notNull(string, "Cannot check a null string for a match");
         if (string.length() < prefix.length()) {
